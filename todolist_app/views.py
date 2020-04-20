f1af69a19d54e505c1fb96f7a918f619f7d15f86
vinodsharma13 <40714256+vinodsharma13@users.noreply.github.com>
2020-04-20 11:13:23 +0530

    uploading all taskmate

---
+++ todolist_app/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from todolist_app.models import TaskList
+from todolist_app.forms import TaskForm
+from django.contrib import messages
+from django.core.paginator import Paginator
+# Create your views here.
+def todolist(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            messages.success(request,("New Task Added!"))
+        return  redirect('todolist')
+    else:
+            all_tasks = TaskList.objects.all()
+            paginator = Paginator(all_tasks, 5)
+            page = request.GET.get('pg')
+            all_tasks = paginator.get_page(page)
+
+            
+            return render(request, 'todolist.html', {'all_tasks': all_tasks})
+    
+def delete_task(request, task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.delete()
+    return redirect(todolist)
+
+def edit_task(request, task_id):
+    if request.method == "POST":
+        task = TaskList.objects.get(pk=task_id)
+        form = TaskForm(request.POST or None, instance = task)
+        if form.is_valid():
+            form.save()
+            messages.success(request,("Task Edited!"))
+            return  redirect('todolist')
+    else:
+            tasks_obj = TaskList.objects.get(pk=task_id)
+            return render(request, 'edit.html', {'task_obj': tasks_obj})
+    
+def complete_task(request, task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.done = True
+    task.save()
+    
+    return redirect(todolist)
+
+def pending_task(request, task_id):
+    task = TaskList.objects.get(pk=task_id)
+    task.done = False
+    task.save()
+    
+    return redirect(todolist)
+
+def index(request):
+    context = {
+        'index_text':"Welcome index Page."
+    }
+    return render(request, 'index.html', context)
+
+def contact(request):
+    context = {
+        'welcome_text':"Welcome  Contact Page"
+    }
+    return render(request, 'contact.html', context)
+
+def about(request):
+    context = {
+        'welcome_text':"Welcome About Page."
+    }
+    return render(request, 'about.html', context)
---
+++ users_app/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .forms import CustomRegisterForm
+
+# Create your views here.
+def register(request):
+    if request.method == "POST":
+        register_form = CustomRegisterForm(request.POST)
+        if register_form.is_valid():
+            #register_form.save(commit=False)
+            register_form.save()
+
+            messages.success(request,("New User Account Created"))
+            return redirect('register')
+         
+    else:
+        register_form = CustomRegisterForm()
+        return render(request, 'register.html', {'register_form': register_form})
+        
